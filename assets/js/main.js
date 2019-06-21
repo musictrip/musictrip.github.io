8cc6cd2365d184fff6f7d8ff1275080414d76b61
Benjamin Wagner <bewagner@uni-bremen.de>
2019-06-21 21:57:44 +0200

    Make search bar work with buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
 
+    // TODO Comment
+
     // quick search regex
     var qsRegex;
+    var buttonFilter;
 
     var $grid = $('.filterable').isotope({
         // options
         itemSelector: '.filter-item',
         percentPosition: true,
         layoutMode: 'fitRows',
-        filter: function() {
-            return qsRegex ? $(this).text().match( qsRegex ) : true;
+        filter: function () {
+            var $this = $(this);
+            var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+            var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
+            return searchResult && buttonResult;
         }
     });
 
@@ -30,32 +36,42 @@ $(document).ready(function () {
 
 // bind filter button click
     $('.filters-button-group').on('click', 'button', function () {
-        var filterValue = $(this).attr('data-filter');
-        // use filterFn if matches value
-
-        filterValue = filterFns[filterValue] || filterValue;
-        $grid.isotope({filter: filterValue});
+        buttonFilter = $(this).attr('data-filter');
+        buttonFilter = filterFns[buttonFilter] || buttonFilter;
+        $grid.isotope();
     });
 
     // use value of search field to filter
-    var $quicksearch = $('.quicksearch').keyup( debounce( function() {
-        qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+    var $quicksearch = $('.quicksearch').keyup(debounce(function () {
+        qsRegex = new RegExp($quicksearch.val(), 'gi');
         $grid.isotope();
-    }, 200 ) );
+    }, 200));
 
 // debounce so filtering doesn't happen every millisecond
-    function debounce( fn, threshold ) {
+    function debounce(fn, threshold) {
         var timeout;
         threshold = threshold || 100;
         return function debounced() {
-            clearTimeout( timeout );
+            clearTimeout(timeout);
             var args = arguments;
             var _this = this;
+
             function delayed() {
-                fn.apply( _this, args );
+                fn.apply(_this, args);
             }
-            timeout = setTimeout( delayed, threshold );
+
+            timeout = setTimeout(delayed, threshold);
         };
     }
+
+
+    $("#searchinput").keyup(function () {
+        $("#searchclear").toggle(Boolean($(this).val()));
+    });
+    $("#searchclear").toggle(Boolean($("#searchinput").val()));
+    $("#searchclear").click(function () {
+        $("#searchinput").val('').focus();
+        $(this).hide();
+    });
 
 });
